e244947be78913db91dbec9d3a77f3aa978cbb04
Christian Morgan Andersen <cmandersen@outlook.com>
2014-02-02 11:55:38 +0100

    RequireJS relative paths

--- app/js/main.js
+++ app/js/main.js
@@ -4,9 +4,9 @@ require.config({
 		angularRoute: "//ajax.googleapis.com/ajax/libs/angularjs/1.2.7/angular-route.min",
 		angularResource: "//ajax.googleapis.com/ajax/libs/angularjs/1.2.7/angular-resource.min",
 		angularCookies: "//ajax.googleapis.com/ajax/libs/angularjs/1.2.7/angular-cookies.min",
-		bootstrap: "app/lib/bootstrap/ui-bootstrap-custom-0.9.0.min",
-		nappMenu: "app/lib/napp-menu/js/rq-angular-napp-menu",
-		base64: "app/lib/base64",
+		bootstrap: "../../../app/lib/bootstrap/ui-bootstrap-custom-0.9.0.min",
+		nappMenu: "../../../app/lib/napp-menu/js/rq-angular-napp-menu",
+		base64: "../../../app/lib/base64",
 	},
 	shim: {
 		"angular": {
