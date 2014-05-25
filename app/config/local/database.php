5861d8facf560af59d17826b6956d14f6fd27202
Christian Morgan Andersen <cmandersen@outlook.com>
2014-05-25 11:23:58 +0200

    Change local db pass

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -32,7 +32,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'test',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
